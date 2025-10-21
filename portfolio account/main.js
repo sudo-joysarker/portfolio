ed153a77e0888e3a93eddd17d97ec1fd6b9cb2a0
sudo-joysarker <joysarkermrist@gmail.com>
2025-10-21 21:39:46 +0600

    Delete portfolio account directory
    delete message

--- portfolio account/main.js
+++
@@ -1,8 +0,0 @@
-// var typed = new Typed(".text", {
-//       strings: ["Web Developer", "Frontend Developer"],
-//       typeSpeed: 100,
-//       backSpeed: 100,
-//       backDelay: 1000,
-//       loop: true
-//     });
-
